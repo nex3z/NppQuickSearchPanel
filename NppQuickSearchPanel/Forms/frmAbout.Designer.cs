27058ac2e853fced1d7d822ca89b22dab4459722
nex3z <litianxing9@gmail.com>
2015-09-09 21:12:47 +0800

    Change the layout.

--- NppQuickSearchPanel/Forms/frmAbout.Designer.cs
+++ NppQuickSearchPanel/Forms/frmAbout.Designer.cs
@@ -28,54 +28,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAbout));
-            this.tlpAbout = new System.Windows.Forms.TableLayoutPanel();
-            this.lblAbout = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnVisitSite = new System.Windows.Forms.Button();
-            this.tlpAbout.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.lblAbout = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // tlpAbout
-            // 
-            this.tlpAbout.ColumnCount = 1;
-            this.tlpAbout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpAbout.Controls.Add(this.lblAbout, 0, 0);
-            this.tlpAbout.Controls.Add(this.panel1, 0, 1);
-            this.tlpAbout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpAbout.Location = new System.Drawing.Point(9, 9);
-            this.tlpAbout.Name = "tlpAbout";
-            this.tlpAbout.RowCount = 2;
-            this.tlpAbout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
-            this.tlpAbout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tlpAbout.Size = new System.Drawing.Size(284, 163);
-            this.tlpAbout.TabIndex = 0;
-            // 
-            // lblAbout
-            // 
-            this.lblAbout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblAbout.Location = new System.Drawing.Point(3, 0);
-            this.lblAbout.Name = "lblAbout";
-            this.lblAbout.Size = new System.Drawing.Size(278, 108);
-            this.lblAbout.TabIndex = 0;
-            this.lblAbout.Text = resources.GetString("lblAbout.Text");
-            // 
-            // panel1
-            // 
-            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panel1.Controls.Add(this.btnClose);
-            this.panel1.Controls.Add(this.btnVisitSite);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 111);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(278, 49);
-            this.panel1.TabIndex = 1;
             // 
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Location = new System.Drawing.Point(200, 24);
+            this.btnClose.Location = new System.Drawing.Point(217, 165);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 1;
@@ -85,7 +46,7 @@
             // 
             // btnVisitSite
             // 
-            this.btnVisitSite.Location = new System.Drawing.Point(3, 24);
+            this.btnVisitSite.Location = new System.Drawing.Point(12, 165);
             this.btnVisitSite.Name = "btnVisitSite";
             this.btnVisitSite.Size = new System.Drawing.Size(75, 23);
             this.btnVisitSite.TabIndex = 0;
@@ -93,12 +54,23 @@
             this.btnVisitSite.UseVisualStyleBackColor = true;
             this.btnVisitSite.Click += new System.EventHandler(this.btnVisitSite_Click);
             // 
+            // lblAbout
+            // 
+            this.lblAbout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblAbout.Location = new System.Drawing.Point(9, 9);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(286, 136);
+            this.lblAbout.TabIndex = 2;
+            this.lblAbout.Text = resources.GetString("lblAbout.Text");
+            // 
             // frmAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 181);
-            this.Controls.Add(this.tlpAbout);
+            this.ClientSize = new System.Drawing.Size(304, 196);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnVisitSite);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -108,19 +80,15 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "About & Help";
-            this.tlpAbout.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tlpAbout;
-        private System.Windows.Forms.Label lblAbout;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnVisitSite;
+        private System.Windows.Forms.Label lblAbout;
 
 
 
