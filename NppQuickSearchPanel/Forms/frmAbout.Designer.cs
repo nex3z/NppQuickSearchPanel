808e071f5d62e58d8b9c90d3883227338d3e08ae
nex3z <litianxing9@gmail.com>
2015-09-20 22:57:50 +0800

    Delete unused region.

--- NppQuickSearchPanel/Forms/frmAbout.Designer.cs
+++ NppQuickSearchPanel/Forms/frmAbout.Designer.cs
@@ -36,9 +36,9 @@
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Location = new System.Drawing.Point(217, 165);
+            this.btnClose.Location = new System.Drawing.Point(217, 152);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(75, 21);
             this.btnClose.TabIndex = 1;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -46,9 +46,9 @@
             // 
             // btnVisitSite
             // 
-            this.btnVisitSite.Location = new System.Drawing.Point(12, 165);
+            this.btnVisitSite.Location = new System.Drawing.Point(12, 152);
             this.btnVisitSite.Name = "btnVisitSite";
-            this.btnVisitSite.Size = new System.Drawing.Size(75, 23);
+            this.btnVisitSite.Size = new System.Drawing.Size(75, 21);
             this.btnVisitSite.TabIndex = 0;
             this.btnVisitSite.Text = "Visit site";
             this.btnVisitSite.UseVisualStyleBackColor = true;
@@ -57,17 +57,17 @@
             // lblAbout
             // 
             this.lblAbout.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblAbout.Location = new System.Drawing.Point(9, 9);
+            this.lblAbout.Location = new System.Drawing.Point(9, 8);
             this.lblAbout.Name = "lblAbout";
-            this.lblAbout.Size = new System.Drawing.Size(286, 136);
+            this.lblAbout.Size = new System.Drawing.Size(286, 126);
             this.lblAbout.TabIndex = 2;
             this.lblAbout.Text = resources.GetString("lblAbout.Text");
             // 
             // frmAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 196);
+            this.ClientSize = new System.Drawing.Size(304, 181);
             this.Controls.Add(this.lblAbout);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnVisitSite);
@@ -75,11 +75,11 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAbout";
-            this.Padding = new System.Windows.Forms.Padding(9);
+            this.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "About & Help";
+            this.Text = "About";
             this.ResumeLayout(false);
 
         }
